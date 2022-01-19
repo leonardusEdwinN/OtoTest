160e5e30367566b72a65b467d320f482b16ba72d
Edwin <osproject78@gmail.com>
2022-01-19 22:07:21 +0700

    Create Update Delete

--- OtoTest/Features/ListPost/ViewModel/ListPostViewModel.swift
+++ OtoTest/Features/ListPost/ViewModel/ListPostViewModel.swift
@@ -59,5 +59,91 @@ class PostViewModel {
     init(post: Post) {
         self.item = post
     }
+    
+    func createNewPost(title: String, content : String, completion: @escaping (PostViewModel) -> Void) {
+        let json: [String: Any] = ["title": title,
+                                   "content": content]
+        
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        
+        var urlRequest = URLRequest(url:  Constants.Urls.urlForGetAllPost())
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = jsonData
+        
+        print("ADD NEW POST DATA FROM JSON :\(json) : \(urlRequest)")
+        Webservice().loadPOST(resource: urlRequest) { (result) in
+            
+            
+            if let postResult = result {
+                do{
+                    let dataDecoder = try JSONDecoder().decode(Post.self, from: postResult)
+                    let postVM = PostViewModel(post: dataDecoder)
+                       completion(postVM)
+                  } catch let parsingError {
+                     print("Error", parsingError)
+                }
+            }
+        }
+        
+    }
+    
+    func updateData(title: String, content : String, id: String, completion: @escaping (PostViewModel) -> Void) {
+        let json: [String: Any] = ["title": title,
+                                   "content": content]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        
+        var urlRequest = URLRequest(url:  Constants.Urls.urlForUpdateDelete(id: "\(id)"))
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "PUT"
+        urlRequest.httpBody = jsonData
+        
+        
+        Webservice().loadPOST(resource: urlRequest) { (result) in
+            
+            
+            if let postResult = result {
+                do{
+                    let dataDecoder = try JSONDecoder().decode(Post.self, from: postResult)
+                    let postVM = PostViewModel(post: dataDecoder)
+                       completion(postVM)
+                  } catch let parsingError {
+                     print("Error", parsingError)
+                }
+            }
+        }
+        
+    }
+    
+    
+    func deleteData(title: String, content : String, id: String, completion: @escaping (PostViewModel) -> Void) {
+        let json: [String: Any] = ["title": title,
+                                   "content": content]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        
+        var urlRequest = URLRequest(url:  Constants.Urls.urlForUpdateDelete(id: "\(id)"))
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "DELETE"
+        urlRequest.httpBody = jsonData
+        
+        
+        Webservice().loadPOST(resource: urlRequest) { (result) in
+            
+            
+            if let postResult = result {
+                do{
+                    let dataDecoder = try JSONDecoder().decode(Post.self, from: postResult)
+                    let postVM = PostViewModel(post: dataDecoder)
+                       completion(postVM)
+                  } catch let parsingError {
+                     print("Error", parsingError)
+                }
+            }
+        }
+        
+    }
 }
 
