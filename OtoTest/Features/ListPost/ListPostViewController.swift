1172c65093f4e26240bdf3f54885df1dc3f65d5d
Edwin <osproject78@gmail.com>
2022-01-19 10:29:15 +0700

    Initialize Project

---
+++ OtoTest/Features/ListPost/ListPostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListPostViewController.swift
+//  OtoTest
+//
+//  Created by Edwin Niwarlangga on 19/01/22.
+//
+
+import Foundation
+import UIKit
+
+
+class ListPostViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
--- OtoTest/OtoTest/Features/ListPost/ListPostViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ListPostViewController.swift
-//  OtoTest
-//
-//  Created by Edwin Niwarlangga on 19/01/22.
-//
-
-import Foundation
